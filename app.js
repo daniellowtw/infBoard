f14d5f94c50149f22d063dddbb9a729d4c8cd802
Dlow <daniellowtw@gmail.com>
2016-01-02 23:59:06 +0000

    Remove unnecessary lines and add comments

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var SocketBroker = require("./app/js/socket_broker");
 var redisPub = require("redis-connection")();
-var redisSub = require("redis-connection")("subscriber");
 var dbClient = require("./lib/db_client");
 var serverSocketBroker = require("./lib/server_socket_broker")
 var port = process.env.PORT || 8080;
 var router = express.Router();
 var api = require("./lib/api");
 
+// register API end point
 router.use("/api", api);
+// Everything else use default middleware.
 router.use(express.static(__dirname + '/public'));
+
 app.use(router);
 
 http.listen(port);
